57962ba963e4b7f00a8dd841b84227193ed8e8a8
SafaElmali <tsafaelmali@gmail.com>
2021-02-16 15:10:02 +0300

    refactor: modify call card

--- components/icons/User.js
+++ components/icons/User.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-function SvgUser(props) {
+function SvgUser({ currentColor = "#222", ...props }) {
   return (
     <svg
       width="1em"
@@ -14,7 +14,7 @@ function SvgUser(props) {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M19 20a1 1 0 11-2 0c0-2.757-2.243-5-5-5s-5 2.243-5 5a1 1 0 11-2 0c0-3.86 3.141-7 7-7s7 3.14 7 7zM12 5c1.103 0 2 .897 2 2s-.897 2-2 2-2-.897-2-2 .897-2 2-2zm0 6c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4z"
-        fill="#222"
+        style={{ fill: currentColor }}
       />
     </svg>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,46 @@
-import { Box, Container, Heading } from "@chakra-ui/react";
 import Head from "next/head";
 import styles from "../styles/home.module.css";
+import * as Icon from "../components/icons";
+import Input from "../components/Input";
 
 const Home = () => {
   return (
-    <Container className={styles.container}>
+    <div>
       <Head>
-        <title>Tahsin Safa Elmali</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Superpeer UI Developer Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Heading>Hello</Heading>
-    </Container>
+      <main>
+        <section className={styles.card}>
+          <div className={styles.callDetails}>
+            <div className={styles.callTitle}>
+              <p>Call with</p>
+              <p>Niloya Kayıkçı</p>
+            </div>
+            <div className={styles.callSchedule}>
+              <div>
+                <Icon.Calendar currentColor={"#3488FA"} />
+                <p>7 Oct, Weds</p>
+              </div>
+              <div>
+                <Icon.Clock currentColor={"#3488FA"} />
+                <p>10:15AM - 10.30AM</p>
+              </div>
+            </div>
+            <p className={styles.callZone}>Timezone: Europe/Amsterdam</p>
+          </div>
+          <div className={styles.video}>
+            <img src="/stock-image.jpg" />
+            <div className={styles.videoControl}></div>
+          </div>
+          <div className={styles.settings}>
+            <Input icon={<Icon.User currentColor={"#3488FA"} />} />
+            <div className={styles.settingsControls}></div>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 };
 
